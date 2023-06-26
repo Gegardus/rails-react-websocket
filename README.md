145c604ead3d3c98d36dc53b1ea3940bcc60728c
Gegardus <stgegard@gmail.com>
2023-06-26 16:08:40 +0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rails-react-websocket
+
+[Go](https://medium.com/@dakota.lillie/using-action-cable-with-react-c37df065f296)
---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ConversationsList from './components/ConversationsList';
+
+function App() {
+  return (
+    <div className="App">
+      <ConversationsList />
+    </div>
+  );
+}
+
+export default App;
